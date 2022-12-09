0faa9bc7e98186841f61c21f59ca9de81b62ce26
Magusest <redikor49@gmail.com>
2022-12-09 19:56:06 +0300

    Настроил сборку

---
+++ webpack.config.js
@@ -0,0 +1,17 @@
+const CopyPlugin = require('copy-webpack-plugin');
+const path = require('path');
+
+module.exports = {
+  entry: './src/main.js',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'build'),
+    clean: true,
+  },
+  devtool: 'source-map',
+  plugins: [
+    new CopyPlugin({
+      patterns: [{from: 'public'}],
+    }),
+  ],
+};
