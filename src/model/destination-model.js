008548b9dd467481a594528063fc81e944a96b30
Magusest <redikor49@gmail.com>
2023-01-10 12:13:11 +0300

    Добовил экран загрушку

--- src/model/destination-model.js
+++ src/model/destination-model.js
@@ -1,6 +1,6 @@
 import { getRandomDestination } from '../mocks/destinations.js';
 
-const DESTINATONS_COUNT = 6;
+const DESTINATONS_COUNT = 10;
 
 export default class DestinationsModel {
   #destinations = Array.from({length: DESTINATONS_COUNT}, getRandomDestination);
--- src/presenter/list-presenter.js
+++ src/presenter/list-presenter.js
@@ -5,6 +5,7 @@ import ListView from '../view/destination-list-view.js';
 import EditView from '../view/edit-form-view.js';
 import PointView from '../view/destination-points-view.js';
 import { offersByType } from '../mocks/destinations.js';
+import NoPointView from '../view/no-point-view.js';
 
 
 export default class ListPresenter {
@@ -21,12 +22,15 @@ export default class ListPresenter {
   init() {
     this.#destinationsList = [...this.#destinationsModel.destinations];
     render(this.#listComponent, this.#listConteiner);
-    render(new SortView, this.#listComponent.element, RenderPosition.BEFOREEND);
-    // render(new NewFormView({trip: this.#destinationsList[0], allOffers: offersByType}), this.#listComponent.element);
-    // render(new EditView({trip: this.#destinationsList[1], allOffers: offersByType}), this.#listComponent.element, RenderPosition.BEFOREEND);
 
-    for (let i = 0; i < this.#destinationsList.length; i++) {
-      this.#renderPoint(this.#destinationsList[i], offersByType);
+    if (this.#destinationsList.every((destination) => destination.isArchive)) {
+      render(new NoPointView, this.#listComponent.element);
+    } else {
+      render(new SortView, this.#listComponent.element, RenderPosition.BEFOREEND);
+      // render(new NewFormView({trip: this.#destinationsList[0], allOffers: offersByType}), this.#listComponent.element);
+      for (let i = 0; i < this.#destinationsList.length; i++) {
+        this.#renderPoint(this.#destinationsList[i], offersByType);
+      }
     }
 
   }
