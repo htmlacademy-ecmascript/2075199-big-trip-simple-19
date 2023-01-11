5c0946aa38924ad85b538cbe2f6abbff203e9812
Magusest <redikor49@gmail.com>
2023-01-11 15:49:56 +0300

    Установил лоадеры

--- src/view/edit-form-view.js
+++ src/view/edit-form-view.js
@@ -115,8 +115,8 @@ function createEditTemplate (trip, allOffers) {
         <div class="event__available-offers">
         ${offers.map((offer) => (`
           <div class="event__offer-selector">
-            <input class="event__offer-checkbox  visually-hidden" id="event-offer-luggage-1" type="checkbox" name="event-offer-${offer.id}" ${trip.offers.includes(offer.id) ? 'checked' : ''}>
-            <label class="event__offer-label" for="event-offer-luggage-1">
+            <input class="event__offer-checkbox  visually-hidden" id="event-offer-luggage-${offer.id}" type="checkbox" name="event-offer-${offer.id}" ${trip.offers.includes(offer.id) ? 'checked' : ''}>
+            <label class="event__offer-label" for="event-offer-luggage-${offer.id}">
               <span class="event__offer-title">${offer.title}</span>
               &plus;&euro;&nbsp;
               <span class="event__offer-price">${offer.price}</span>
