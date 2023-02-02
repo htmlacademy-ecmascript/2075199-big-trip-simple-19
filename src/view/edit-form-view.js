5a9d1ad395586c1ee962031c02815916aedc6365
Magusest <redikor49@gmail.com>
2023-02-02 16:42:37 +0300

    Убрал лишние запросы

--- src/view/edit-form-view.js
+++ src/view/edit-form-view.js
@@ -1,5 +1,5 @@
 import AbstractStatefulView from '../framework/view/abstract-stateful-view.js';
-import { fullDateFrom, fullDateTo, machineDateTimeFrom, machineDateTimeTo } from '../utils/utils.js';
+import { fullDateFrom, fullDateTo } from '../utils/utils.js';
 import flatpickr from 'flatpickr';
 import 'flatpickr/dist/flatpickr.min.css';
 
