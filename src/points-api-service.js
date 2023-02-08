bb21da47c504d50ce3e2ea2c0f2106a79cb4e657
Magusest <redikor49@gmail.com>
2023-02-08 11:10:15 +0300

    Добавил обновления задач

--- src/points-api-service.js
+++ src/points-api-service.js
@@ -26,7 +26,7 @@ export default class PointsApiService extends ApiService {
       url: `points/${point.id}`,
       method: Method.PUT,
       body: JSON.stringify(this.#adaptToServer(point)),
-      headers: new Headers({'Content-Type': 'aplication/json'}),
+      headers: new Headers({'Content-Type': 'application/json'}),
     });
 
     const parsedResponse = await ApiService.parseResponse(response);
